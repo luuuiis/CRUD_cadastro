a781adc43065a399b4c11aca960e8c4b42b6dded
Luís Fillipe <luis.digitalti@gmail.com>
2026-06-09 12:17:45 -0300

    now added

--- teste-projeto1/src/main/java/com/luuuiis/teste_projeto1/controller/ProdutoController.java
+++ teste-projeto1/src/main/java/com/luuuiis/teste_projeto1/controller/ProdutoController.java
@@ -1,9 +1,11 @@
 package com.luuuiis.teste_projeto1.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,6 +35,17 @@ public class ProdutoController {
     @PostMapping 
     public Produto adicionar(@RequestBody Produto produto){ 
         return produtoService.adicionar(produto);
+    }
+
+    @DeleteMapping("/{id}") // ex: "/1" -> localhost:8080/produtos/1
+    public String deletar(@PathVariable Integer id){
+        produtoService.deletar(id);
+        return "Produto " + id + " deletado com sucesso";
+    }
+
+    @PutMapping("/{id}")
+    public Produto atualizar(@RequestBody Produto produto, @PathVariable Integer id){
+        return produtoService.atualizar(id, produto);
     }
 
 }
